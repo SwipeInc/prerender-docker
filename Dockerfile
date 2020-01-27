b633b08a25fd2edcdeb7499d24d47792eb9ebc3e
Jacques Millard <jacques@millard.za.org>
2020-01-27 15:31:35 +0200

    add nginx update server config

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.13
+FROM node:12
 MAINTAINER Magnet.me
 
 EXPOSE 3000
@@ -12,6 +12,7 @@ RUN \
   wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
   echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list && \
   apt-get update && \
+  apt-get upgrade -y && \
   apt-get install -y google-chrome-stable && \
   apt-get clean && \
   rm -rf /var/lib/apt/lists/*
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const options = {
 	pageDoneCheckInterval: process.env.PAGE_DONE_CHECK_INTERVAL || 500,
 	pageLoadTimeout: process.env.PAGE_LOAD_TIMEOUT || 20000,
 	waitAfterLastRequest: process.env.WAIT_AFTER_LAST_REQUEST || 250,
-	chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ],
+	chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage' ],
 };
 console.log('Starting with options:', options);
 
@@ -20,12 +20,19 @@ const server = prerender(options);
 
 server.use(log);
 server.use(healthcheck('_health'));
-server.use(forwardHeaders);
-server.use(forwardResponseHeaders);
+//server.use(forwardHeaders);
+//server.use(forwardResponseHeaders);
 server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(removePrefetchTags);
 server.use(prerender.httpHeaders());
+server.use(prerender.sendPrerenderHeader());
+if (process.env.DISABLE_CACHE !== "1") {
+    console.log("S3 Cache enabled");
+    server.use(require('prerender-aws-s3-cache'));
+} else {
+    console.log("S3 Cache disabled");
+}
 if (process.env.DEBUG_PAGES) {
 	server.use(consoleDebugger);
 }
